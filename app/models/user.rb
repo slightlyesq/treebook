25c576616a9f827158fc12a957bbacfaa8a81a0e
Paden Woodruff <padenwoodruff@gmail.com>
2013-11-20 10:54:16 -0500

    Updated frontend for the new authentication system
    Added simple_form
    Updated several forms to bootstrap/simple_form
    Added lgoin/logout/register/profile links to our nav
    Added a collection select to our status form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :first_name, :last_name,:email, :profile_name,
-  				  :password, :password_confirmation, :remember_me
+  attr_accessible :first_name, :last_name,:email, :profile_name, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  
+  has_many :statuses
+  
+  def full_name
+  	first_name + " " + last_name
+  end
 end
